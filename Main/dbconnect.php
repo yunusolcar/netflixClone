c38f5f0808f3270cc5efdc43ec357c1d64ef7c9d
yunusolcar <yunusolcar@hotmail.com>
2022-09-10 21:43:56 +0300

    Db connect process ✓

--- Main/dbconnect.php
+++ Main/dbconnect.php
@@ -2,5 +2,12 @@
 
 $connectDb = new PDO("mysql:host=localhost;dbname=netflixdb;charset=utf8", "admin", "1234");
 $connectDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connectDb->query("set character set utf8");
 
+$dbquery = $connectDb->query("select * from customers where id='1'");
+$dbrow = $dbquery->fetch(PDO::FETCH_ASSOC);
+
+foreach ($dbrow as $dbdata) {
+            echo $dbdata . "<br>";
+}
 ?>
