8f6d04d37579ad9893f8c3d5a18f5d04a197e57e
yunusolcar <yunusolcar@hotmail.com>
2022-09-17 21:22:47 +0300

    new features

--- Main/login.php
+++ Main/login.php
@@ -1,49 +1,60 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 
 <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Netflix - Giriş</title>
-            <link rel="stylesheet" href="../Css/sign_in.css">
-            <link rel="icon" href="../Css/Images/logo.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflix - Giriş</title>
+    <link rel="stylesheet" href="../Css/sign_in.css">
+    <link rel="icon" href="../Css/Images/logo.png">
 </head>
 
 <body>
 
-<body>
-    <div class="navbar" id="navbar">
-        <img src="../Css/Images/netflix-logo-png-2564.png" alt="" class="logo" height="25px" style="padding:18px;">
-    </div>
+    <body>
+        <div class="navbar" id="navbar">
+            <img src="../Css/Images/netflix-logo-png-2564.png" alt="" class="logo" height="25px" style="padding:18px;">
+        </div>
 
-            <div class="login-div">
-                        <div class="login">
-                                    <div class="app-title">
-                                                <h1 style="text-align:center;">Giriş Yap</h1>
-                                    </div>
-                                    <form action="loginCheck.php" method="post">
-                                                <div class="login-div">
-                                                            <div class="input-text">
-                                                                        <input type="text" name="username" class="login-field" placeholder="Kullanıcı Adınız" id="login-name" required>
-                                                                        <label class="log-field-icon" for="login-name"></label>
-                                                            </div>
-                                                            <div class="input-text">
-                                                                        <input type="password" name="password" class="login-field" placeholder="Şifre" id="login-pass" required>
-                                                                        <label class="log-field-icon" for="login-pass"></label>
-                                                            </div>
-                                                            <div>
-                                                                        <button type="submit" name="kayit" class="submit-button">Giriş Yap</button>
-                                                            </div>
-                                                </div>
-                                    </form>
-                                  <a href="register.php"><button class="submit-button">Netflix' e Kayıt Ol</button>
+        <div class="login-div">
+            <div class="login">
+                <div class="app-title">
+                    <h1 style="text-align:center;">Sing In</h1>
+                </div>
+                <form action="loginCheck.php" method="post">
+                    <div class="login-div">
+                        <div class="input-text">
+                            <input type="text" name="username" class="login-field" placeholder="Name or E-Mail" id="login-name" required>
+                            <label class="log-field-icon" for="login-name"></label>
                         </div>
+                        <div class="input-text">
+                            <input type="password" name="password" class="login-field" placeholder="Password" id="login-pass" required>
+                            <label class="log-field-icon" for="login-pass"></label>
+                        </div>
+                        <div>
+                            <button type="submit" name="kayit" class="submit-button">Sign In</button>
+                        </div>
+                    </div>
+                </form>
+                <div class="signup" style="text-decoration:none ; color:gray; padding-top: 20px;">
+                  New to Netflix?    
+                        <a href="register.php" style="color:white">Sign up now.</a>
+                    </div>
+                    <div class="more">
+                    <p style="font-size:15px; color:gray">
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="#">Learn more.</a> 
+                    </p>
+                </div>
+                </div>
             </div>
-  
-</body>
+        </div>
+
+
+
+
+
+    </body>
 
 </html>
